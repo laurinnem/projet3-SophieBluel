a27197daa2a48a2f48dbbc8581d2e43ad0985a5a
laurinnem <122321024+laurinnem@users.noreply.github.com>
2023-04-06 11:09:17 +0200

    add js and css for modal (ongoing)

--- FrontEnd/edit.js
+++ FrontEnd/edit.js
@@ -9,6 +9,15 @@ const modalForm = document.getElementById("modalForm");
 const btnModal = document.getElementById("btnModal");
 const closeModal = modal.style.display = "none";
 const deleteGalleryButton = document.getElementById("sup");
+const boutonFormAjoutPhoto = document.getElementById("btn-ajoutPhoto");
+const modalAjoutPhoto = document.getElementById('modalAjoutPhoto');
+
+
+//récupération des catégories des travaux (importer de works.js?)
+let categories = await fetch ('http://localhost:5678/api/categories');
+categories = await categories.json();
+const reponseCategories = JSON.stringify(categories);
+console.log(categories);
 
 
 //display editMode (boutton modifier/logout) et cache filtres
@@ -45,7 +54,7 @@ loginLogout.addEventListener("click", function(event) {
 let works = await fetch ('http://localhost:5678/api/works');
     works = await works.json();
     const reponseWorks = JSON.stringify(works);
-    //window.localStorage.setItem("works", reponseWorks);
+
         
 function genererModalGallery(works) {
     for (let i = 0; i < works.length; i++) {
@@ -73,6 +82,11 @@ btnEdit.addEventListener("click", function(event) {
 
 
 //e click sur croix pour fermer modale
+const xMarkIcon = document.getElementById("xMarkIcon");
+xMarkIcon.addEventListener("click", function(event) {
+    event.preventDefault();
+    closeModal;
+});
 //e click en dehors de la modale x fermer modale
 
 
@@ -80,8 +94,7 @@ btnEdit.addEventListener("click", function(event) {
 
 //e click sur "supprimer la gallerie" x supprimer tout ds API => doit ê fonctionnel??)
 
-//e click sur bouton "ajouter une photo" x changer config modale2 (mm id x éléments modal2-querySelectorAll)/ 
-//+ créer éléments non présents ds html si besoin
+//e click sur bouton modale x changer config 
 btnModal.addEventListener("click", function(event) {
     event.preventDefault();
     if (btnModal.innerText === "Ajouter une photo") {
@@ -90,8 +103,49 @@ btnModal.addEventListener("click", function(event) {
         deleteGalleryButton.style.display = "none";
         btnModal.innerText = "Valider";
         btnModal.style.backgroundColor = "#A7A7A7";
+
+        
+    
         //récup liste catégories (fetch) x champs categories + possibilité ajout nouvelle categorie?
-            //e click sur ajouter photo x charger new photo + afficher new photo ds modale
+        
+        //ajout liste ds champs 
+
+
+            
+            //e sur bouton "+ Ajout photo" x charger photo nouveau projet 
+        boutonFormAjoutPhoto.addEventListener("click", function(event) {
+            const input = document.createElement("input");
+            input.type = "file";
+            input.accept = "image/jpg, image/png,image/jPEG";
+            input.click();
+            /* vérifie taille et type de fichier */
+            input.addEventListener("change", async function () {
+                const image = input.files[0];
+             if (image.type !== "image/jpg" && image.type !== "image/png" && image.type !== "image/jpeg") {
+                 alert("jpg ou png obligatoire");
+                 return;
+             }
+             if (image.size > 4 * 1024 * 1024) {
+                 alert("La taille maximum du fichier est de 4mo");
+                 return;
+             }
+             /* affichage ds modale de la nouvelle photo, à la place du contenu de div#modalAjoutPhoto */
+             
+             modalAjoutPhoto.innerHTML = "";
+             
+             const reader = new FileReader();
+             reader.readAsDataURL(image);
+             reader.onload = function () {
+                image.src = reader.result;
+                const newImage = document.createElement("img"); 
+                newImage.className = "previewPhoto";
+                newImage.src = image.src;
+                modalAjoutPhoto.appendChild(newImage);
+             };
+             
+          });
+
+        });
 
             //si tt les champs remplis: changer btnModal à "#1D6154"
 
@@ -106,6 +160,8 @@ btnModal.addEventListener("click", function(event) {
         //nouveau projet doit s'afficher sans recharger la page (où et comment actualiser DOM?)
     }
 });
+
+
 
 
 
