b8c227114f2f11e2583f89c3e86c7feed10cd0ae
laurinnem <122321024+laurinnem@users.noreply.github.com>
2023-05-03 10:42:17 +0200

    add regex x login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,4 +1,5 @@
 //charge DOM content avant d'éxécuter la fonction (script ds head)
+
 document.addEventListener('DOMContentLoaded', function (event) {
     const seConnecter = document.querySelector("#btn-seConnecter");
 
@@ -7,33 +8,63 @@ document.addEventListener('DOMContentLoaded', function (event) {
         const email = document.querySelector("[name=email]").value;
         const password = document.querySelector("[name=password]").value;
 
-        let response = fetch("http://localhost:5678/api/users/login", {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password
-            })
-        })
+        const regExEmail = (value) => {
+            return /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/.test(value);
+        };
 
-            .then(function (response) {
-                if (response.status === 200) {
-                    return response.json();
-                } else {
-                    alert("Erreur dans l'identifiant ou le mot de passe");
-                }
+        const regExPassword = (value) => {
+            return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/.test(value);
+        };
+
+        function emailControle() {
+            if (regExEmail(email)) {
+                return true;
+            } else {
+                alert("l'email n'est pas valide");
+                return false;
+            }
+        };
+
+        function passwordControle() {
+            if (regExPassword(password)) {
+                return true;
+            } else {
+                alert("le mot de passe n'est pas valide");
+                return false;
+            }
+        };
+
+        if (emailControle() && passwordControle()) {
+
+            let response = fetch("http://localhost:5678/api/users/login", {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password
+                })
             })
 
-            .then(function (userInfo) {
-                if (userInfo) {
-                    window.sessionStorage.setItem("token", JSON.stringify(userInfo));
-                    window.sessionStorage.setItem("token", userInfo.token);
-                    window.location.replace("index.html");
-                }
-            });
+                .then(function (response) {
+                    if (response.status === 200) {
+                        return response.json();
+                    } else {
+                        alert("Erreur dans l'identifiant ou le mot de passe");
+                    }
+                })
+
+                .then(function (userInfo) {
+                    if (userInfo) {
+                        window.sessionStorage.setItem("token", JSON.stringify(userInfo));
+                        window.sessionStorage.setItem("token", userInfo.token);
+                        window.location.replace("index.html");
+                    }
+                });
+        };
     });
 });
+
 
 
